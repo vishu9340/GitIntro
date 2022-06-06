1a9e96828050b3fe1b5612d716957b70db0ea53b
Vishwas <vishwas.shukla98@gmail.com>
2022-06-06 13:35:54 +0530

    this is my first git commit

---
+++ MyFirstApp.java
@@ -0,0 +1,8 @@
+public class MyFirstApp {
+	
+	// comment added in my-new-branch
+	// This is an important comment
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
